c1e9916555d4903f8434658f2e66acbb265e75d6
Basil Veerman <bveerman@uvic.ca>
2016-03-14 12:31:15 -0700

    Add zipped shapefile opening using FileReader.readAsArrayBuffer. Add README.

---
+++ README.md
@@ -0,0 +1,5 @@
+# leaflet-local-shapefile
+
+This is a testing ground for incorporating local shapefiles into a leaflet map.
+
+http://basilveerman.github.io/leaflet-local-shapefile
--- script.js
+++ script.js
@@ -1,29 +1,53 @@
-addButton = function (map) {
-  var div = L.DomUtil.create('form', 'bgroup');
-  var uploadButton = L.DomUtil.create('input', 'uploadShape', div);
-  uploadButton.type = "file";
-  uploadButton.id = "input";
-  uploadButton.onchange = function() {
-    var file = document.getElementById("input").files[0];
-    handleFile(file);
-  }
-
-  // Makes the 'input' button look better
-  var doneButton = L.DomUtil.create('button', "btn  btn-primary span3", div);
-  doneButton.type = "button";
-  doneButton.innerHTML = "Upload Zipped Shapefile";
-  L.DomEvent.addListener(doneButton, "click", function() {
-      uploadButton.click();
+var readerLoad = function(e) {
+  // e.target.result === reader.result
+  console.log(e.target.result.byteLength);
+  shp(e.target.result).then(function (geojson) {
+    console.log(geojson);
   });
+}
 
+var handleZippedShapefile = function (file) {
+  var reader = new FileReader();
+  reader.onload = readerLoad;
+  reader.readAsArrayBuffer(file);
+}
+
+var makeButtonOverlay = function (div, b, title) {
+  var overlayButton = L.DomUtil.create('button', "btn  btn-primary span3", div);
+  overlayButton.type = "button";
+  overlayButton.innerHTML = title;
+  L.DomEvent.addListener(overlayButton, "click", function() {
+      b.click();
+  });
+  return overlayButton;
+};
+
+var makeUploadInput = function (div, then) {
+  var uploadButton = L.DomUtil.create('input', div);
+  uploadButton.type = "file";
+  uploadButton.style="display:none;";
+  uploadButton.onchange = function() {
+    var file = uploadButton.files[0];
+    then(file);
+  }
+  return uploadButton;
+};
+
+addButton = function (handler, title) {
+  // Creates container div, input button
+  var div = L.DomUtil.create('form', 'bgroup');
+  var uploadButton = makeUploadInput(div, handler);
+
+  // Makes the 'input' button look less bad
+  var overlayButton = makeButtonOverlay(div, uploadButton, title);
   return div;
 };
 
-var NewButton = L.Control.extend({ //creating the buttons
+var zippedShapeControl = L.Control.extend({ //creating the buttons
   options: {
     position: 'topright'
   },
-  onAdd: addButton
+  onAdd: addButton.bind(this, handleZippedShapefile, "Upload Zipped Shapefile")
 });
 
-m.addControl(new NewButton());
+m.addControl(new zippedShapeControl());
